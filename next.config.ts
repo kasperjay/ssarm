1caee7a0b38397e5986d6d61cf9c7389c7c53158
kasperjay <kasperjaylin@gmail.com>
2026-05-04 23:38:53 -0500

    chore: clean up duplicate files and overwrite originals

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["spectral-soundworks-kkkkrash.zocomputer.io", "kkkkrash.zo.computer"],
   images: {
     remotePatterns: [
       {
